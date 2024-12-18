f0231d4b9cb436d1b4fed4ec63523262edc88e24
Andreas Zimmerer <andreas.zimmerer@tum.de>
2024-12-18 12:10:01 +0100

    fixes

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Checkout dotfiles. Existing files will be renamed with .bak in the end
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 'dotfiles' 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} {}.bak
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout dotfiles 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} {}.bak
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout dotfiles
 
 # Hide files we are not explicitly tracking yet.
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
